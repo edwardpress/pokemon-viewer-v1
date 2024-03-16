b27062d8f86ff83923dd51f0ac549fad858de623
edwardchoh <edwardchoh@gmail.com>
2024-03-16 14:32:03 +0800

    lazy loading module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path:  '',   redirectTo: '/dashboard', pathMatch: 'full'},
-  {path: 'dashboard' , component: DashboardComponent},
-  // {path: 'team' , component: ContactDetailComponent}
+  {path:  '',   redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: 'dashboard',  loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)}
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 
 @NgModule({
   declarations: [DashboardComponent],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
